7f45783e34eac05e0f9a513fb45441a692a170a1
Thiago Almeida <64435583+thiagocalm@users.noreply.github.com>
2024-02-14 16:40:19 -0300

    inserting occupation level indicator

--- scripts/X_functions_indicators.R
+++ scripts/X_functions_indicators.R
@@ -46,14 +46,7 @@ df |>
   arrange(idade_quinquenal)
 
 
-# Indicador 1 - Taxa de participacao --------------------------------------
-
-## Parameters
-peso = "peso"
-grupo_etario = "idade_quinquenal"
-coorte_nascimento = "coorte_quinquenal"
-PEA = "PEA"
-tipo = c("total","sexo","sexo e raca","sexo e escolaridade")
+# Indicator 1 - Labor force participation rate ----------------------------------
 
 ## Function
 
@@ -220,6 +213,212 @@ labor_force_rates <- function(
 
     num <- df |>
       filter(PEA == 1) |>
+      summarise(
+        numerador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo, escolaridade)
+      ) |>
+      arrange(grupo_etario)
+
+    # Denominator
+
+    den <- df |>
+      summarise(
+        denominador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo, escolaridade)
+      ) |>
+      arrange(grupo_etario)
+
+    # join
+
+    table <- num |>
+      left_join(
+        den,
+        by = c("coorte_nascimento","grupo_etario", "sexo", "escolaridade"),
+        keep = FALSE
+      ) |>
+      mutate(across(everything(),~ round(.x,0))) |>
+      mutate(taxa = numerador/denominador) |>
+      mutate(
+        sexo = factor(sexo, levels = c(2,1), labels = c("Feminino","Masculino")),
+        escolaridade = factor(
+          escolaridade,
+          levels = c(1,2,3,4,9),
+          labels = c("Menos que fundamental completo","Fundamental completo","Médio completo","Superior completo","NR")
+        )
+      )
+  }
+
+  # output
+  return(table)
+}
+
+# Indicator 2 - Ocupation level --------------------------------------
+
+## Function
+
+ocupation_level <- function(
+    peso = "peso",
+    grupo_etario = "idade_quinquenal",
+    coorte_nascimento = "coorte_quinquenal",
+    PO = "PO",
+    tipo # = c("total","sexo","sexo e raca","sexo e escolaridade")
+){
+
+  if(is.null(tipo)){
+    stop("You need for describing wich type of tabulation you want before...")
+  }
+
+  if(tipo == "total"){
+
+    # Dataframe
+    df <- get(glue::glue("df")) |>
+      select(
+        peso = all_of(peso),
+        grupo_etario = all_of(grupo_etario),
+        coorte_nascimento = all_of(coorte_nascimento),
+        PO = all_of(PO)
+      )
+
+    # Numerator
+
+    num <- df |>
+      filter(PO == 1) |>
+      summarise(
+        numerador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario)
+      ) |>
+      arrange(grupo_etario)
+
+    # Denominator
+
+    den <- df |>
+      summarise(
+        denominador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario)
+      ) |>
+      arrange(grupo_etario)
+
+    # join
+
+    table <- num |>
+      left_join(
+        den,
+        by = c("coorte_nascimento","grupo_etario"),
+        keep = FALSE
+      ) |>
+      mutate(across(everything(),~ round(.x,0))) |>
+      mutate(taxa = numerador/denominador)
+  } if(tipo == "sexo"){
+
+    # Dataframe
+    df <- get(glue::glue("df")) |>
+      select(
+        peso = all_of(peso),
+        grupo_etario = all_of(grupo_etario),
+        coorte_nascimento = all_of(coorte_nascimento),
+        PO = all_of(PO),
+        sexo = "sexo"
+      )
+
+    # Numerator
+
+    num <- df |>
+      filter(PO == 1) |>
+      summarise(
+        numerador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo)
+      ) |>
+      arrange(grupo_etario)
+
+    # Denominator
+
+    den <- df |>
+      summarise(
+        denominador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo)
+      ) |>
+      arrange(grupo_etario)
+
+    # join
+
+    table <- num |>
+      left_join(
+        den,
+        by = c("coorte_nascimento","grupo_etario", "sexo"),
+        keep = FALSE
+      ) |>
+      mutate(across(everything(),~ round(.x,0))) |>
+      mutate(taxa = numerador/denominador) |>
+      mutate(
+        sexo = factor(sexo, levels = c(2,1), labels = c("Feminino","Masculino"))
+      )
+  } if(tipo == "sexo e raca"){
+
+    # Dataframe
+    df <- get(glue::glue("df")) |>
+      select(
+        peso = all_of(peso),
+        grupo_etario = all_of(grupo_etario),
+        coorte_nascimento = all_of(coorte_nascimento),
+        PO = all_of(PO),
+        sexo = "sexo",
+        cor_raca = "cor_raca"
+      )
+
+    # Numerator
+
+    num <- df |>
+      filter(PO == 1) |>
+      summarise(
+        numerador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo,cor_raca)
+      ) |>
+      arrange(grupo_etario)
+
+    # Denominator
+
+    den <- df |>
+      summarise(
+        denominador = sum(peso),
+        .by = c(coorte_nascimento, grupo_etario, sexo, cor_raca)
+      ) |>
+      arrange(grupo_etario)
+
+    # join
+
+    table <- num |>
+      left_join(
+        den,
+        by = c("coorte_nascimento","grupo_etario", "sexo", "cor_raca"),
+        keep = FALSE
+      ) |>
+      mutate(across(everything(),~ round(.x,0))) |>
+      mutate(taxa = numerador/denominador) |>
+      mutate(
+        sexo = factor(sexo, levels = c(2,1), labels = c("Feminino","Masculino")),
+        cor_raca = factor(
+          cor_raca,
+          levels = c(1,2,3,4,5,99),
+          labels = c("Branco","Preto","Indígena","Amarelo","Pardo","NR")
+        )
+      )
+  } if(tipo == "sexo e escolaridade"){
+
+    # Dataframe
+    df <- get(glue::glue("df")) |>
+      select(
+        peso = all_of(peso),
+        grupo_etario = all_of(grupo_etario),
+        coorte_nascimento = all_of(coorte_nascimento),
+        PO = all_of(PO),
+        sexo = "sexo",
+        escolaridade = "escolaridade"
+      )
+
+    # Numerator
+
+    num <- df |>
+      filter(PO == 1) |>
       summarise(
         numerador = sum(peso),
         .by = c(coorte_nascimento, grupo_etario, sexo, escolaridade)
